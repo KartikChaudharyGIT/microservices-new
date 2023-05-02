f417def1c8f902e66141fecae56fd432f4e96cb5
KRTK <kartikchaudhary89@gmail.com>
2023-05-03 04:03:43 +0530

    feature/Service-Discovery -Build fixed -Formatted depreciated annotation

--- product-service/src/main/java/com/microservices/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservices/productservice/ProductServiceApplication.java
@@ -2,9 +2,10 @@ package com.microservices.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
